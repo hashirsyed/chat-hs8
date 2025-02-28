6109479898b86fd92afe0318d720dfbfd3b9fc3f
Hashir Syed <123250183+hashirsyed@users.noreply.github.com>
2025-02-28 12:09:20 +0500

    Changes made in App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React, { useState } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [isVisible, setIsVisible] = useState(true);
+  const [inputValueF, setInputValueF] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => {
+    setInputValueF(inputValue)
     setIsVisible(!isVisible);
+
   };
   return (
     <>
@@ -16,12 +19,13 @@ function App() {
       <p>Assalam-o-Alaikum ! I am HS8 Chatbot . What is your name ?</p>
     </div>
     <div className='mes mes-1' style={{ display: isVisible ? "block" : "none" }}>
-      <p>How are you {inputValue}</p>
+      <p>How are you {inputValueF} ?</p>
     </div>
+    <div className='chat-1'>
     <input
      placeholder='Write your Name' className='chat' type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
      <button type='submit' onClick={toggleVisibility}>Submit</button>
-    
+     </div>
     
 
     
